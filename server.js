412909d8e0d3707e01d2fab63e1d7c559b20bd89
Matt Moore <mattmoore2014@gmail.com>
2023-04-03 19:19:18 -0600

    added some const to the server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const routes = require('./controllers');
+const express = require('./express'); 
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+const auth = require('./utils/auth')
+
+const sequelize = require('./connection'); 
+
+const app = express(); 
+const PORT = process.env.PORT || 3306; 
+
+

98ffbf515852a343aff400bcb1bf87c15fe9d560
Danny <dahneel@gmail.com>
2023-04-06 19:32:50 -0600

    added to js files in public directory

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 
-// render login page
- app.get('/login',(req, res) => {
-    res.render('login');
- });
+app.get('/login',(req, res) => {
+   res.render('login');
+});
 
- app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-  
-// check user and password
+app.post('/login', (req, res) => {
+   const { email, password } = req.body;
    if (email === 'email' && password === 'password') {
    res.redirect('/getquote');
    } else {
    res.render('login', { error: 'Invalid email or password' });
-    }
+   }
 });

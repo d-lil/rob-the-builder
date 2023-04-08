6bb01b59c57abc68a5bce8680070e45da066c321
sanmcc <120269286+sanmcc@users.noreply.github.com>
2023-04-08 13:12:22 -0600

    corrected login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,26 @@
-const express = require('express');
-const app = express();
-
-// render login page
- app.get('/login',(req, res) => {
-    res.render('login');
- });
-
- app.post('/login', (req, res) => {
-    const { email, password } = req.body;
+const login = async (username, password) => {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    });
   
-// check user and password
-   if (email === 'email' && password === 'password') {
-   res.redirect('/getquote');
-   } else {
-   res.render('login', { error: 'Invalid email or password' });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
-});
+  };
+  
+  const handleLogin = () => {
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+  
+    login(username, password);
+  };
+  
+  document.querySelector('#login-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    handleLogin();
+  });
+  
